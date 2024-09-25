009af5de55ecd29261b4298443a9ac13c1410720
FootLettuce26 <caleb.weigt@cuw.edu>
2024-09-25 11:00:57 -0500

    HW #9
    allowed for one argument in if condition, only parser

--- main.rkt
+++ main.rkt
@@ -4,7 +4,7 @@
 (require "utils.rkt")
 (require "interpret.rkt")
 
-(define var-scope
+(define var-env
   '((a 1) (b 2) (x 5))
   )
 
@@ -12,7 +12,7 @@
 
 (define execute
   (lambda (code)
-    (blaaade-interpreter (blaaade-parser code) var-scope)
+    (blaaade-interpreter (blaaade-parser code) var-env)
     )
   )
 
@@ -27,5 +27,9 @@
 
 ;(math-exp (num-exp 1) (op +) (num-exp 2))
 ;(execute '(call (function (x + 1) x) a))
-(blaaade-parser '(call (function (x) (x * 2)) a))
-(execute '(call (function (x) (x * 2)) a))
+
+;(blaaade-parser '(call (function (x) (x * 2)) a))
+;(execute '(call (function (x) (x * 2)) a))
+
+(blaaade-parser '(ask (a == 1) b x))
+
