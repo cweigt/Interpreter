cf9d20253fe9957bf454d538f80e1dd7f824f17e
FootLettuce26 <caleb.weigt@cuw.edu>
2024-10-05 08:33:18 -0500

    Homework #11
    Adjusted queue to push to top-scope

--- interpret.rkt
+++ interpret.rkt
@@ -9,7 +9,7 @@
       ((eq? (car parsed-code) 'var-exp) (var-exp-helper parsed-code env))
       ((eq? (car parsed-code) 'num-exp) (cadr parsed-code))
       ;(app-exp (func-exp ((parmas x) (params y)) (body-exp (math-exp
-      ;(var-exp x) (op +) (var-exp y)) ((var-exp a) (var-exp b)))
+;(var-exp x) (op +) (var-exp y)) ((var-exp a) (var-exp b)))
       ((and (eq? (car parsed-code) 'app-exp)
             (eq? (length (cadr (cadr parsed-code)))
             (length (caddr parsed-code))))
@@ -25,12 +25,45 @@
          (blaaade-interpreter (cadr (caddr (cadr parsed-code))) local_env)
          )
        )
+      ((eq? (car parsed-code) 'out-exp)
+       (displayln (blaaade-interpreter (cadr parsed-code) env)))
+      ;(post-exp (var-exp c) (num-exp 2))
+      ;(((f 2))((a 1)(b 2)(x 5))) -> (((c 2)(f 2))((a 1)(b 2)(x 5)))
+      ;for some reason set wasn't working, so I just got rid of it and returned it normally
+      ((eq? (car parsed-code) 'post-exp)
+       (if (env-contains-name? (cadr (cadr parsed-code)) env)
+           (println "Cannot declare used variable.")
+           (let
+               ((new-pair
+           (list
+            (cadr (cadr parsed-code))
+            (blaaade-interpreter (caddr parsed-code) env))))
+             (if (null? env) (list (list new-pair))
+                 (cons (cons new-pair (car env)) (cdr env))
+                 ))))
+      ;(queue-exp exp1 exp2 exp3...)
+      ;execute exp1
+      ;execute (queue-exp exp2 exp3 ...)
+      ;execuete (queue-exp exp_n) = > execute exp_n
+      ;'(queue-exp (post-exp (var-exp c) (num-exp 2)) (var-exp c))
+      ;queue does the real work, new-env is giong to be a call to post-exp to retrieve that upated environment
+      ;then cons together
+      ((eq? (car parsed-code) 'queue-exp)
+       (cond
+         ((eq? (length parsed-code) 2)
+          (blaaade-interpreter (cadr parsed-code) env))
+         (else
+          (let ((new-env (blaaade-interpreter (cadr parsed-code) env)))
+          (blaaade-interpreter (cons 'queue-exp (cddr parsed-code)) new-env)))))
+      
       ((eq? (car parsed-code) 'math-exp) (math-exp-helper parsed-code env))
-      ;(ask-exp (boolean-exp (var-exp a) (op ==) (num-exp 1)) (true-exp (var-exp b)) (false-exp (var-exp x)))
+      ;'(ask-exp (boolean-exp (var-exp a) (op ==) (num-exp 1))
+      ;(true-exp (var-exp b)) (false-exp (var-exp x)))
       ((eq? (car parsed-code) 'ask-exp)
        (if (blaaade-interpreter (cadr parsed-code) env)
            (blaaade-interpreter (cadr (caddr parsed-code)) env)
-           (blaaade-interpreter (cadr (cadddr parsed-code)) env)))
+           (blaaade-interpreter (cadr (cadddr parsed-code)) env)
+           ))
       ;(boolean-exp (op !) (var-exp a))
       ((and (eq? (car parsed-code) 'boolean-exp)
             (eq? (car (cadr parsed-code)) 'op))
@@ -86,7 +119,7 @@
       )
     )
   )
-;(boolean-exp (num-exp 1) (op >) (num-exp 2))... ignore this dog water comment
+;(math-exp (num-exp 1) (op +) (num-exp 2))
 (define boolean-exp-helper
   (lambda (parsed-code env)
     (boolean-helper (cadr (caddr parsed-code))
